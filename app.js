c25f34a644d5cfcf1da704d79f049a3debec8cd1
Moise <mioseloor2508@gmail.com>
2023-06-07 00:50:40 -0500

    version final

--- app.js
+++ app.js
@@ -16,3 +16,23 @@ function updateClock(){
 }
 
 updateClock();
+
+//agregacion de js para la version de 1.3
+setInterval(() => { //Ejecutar fecha cada segundo
+    let fecha = new Date();
+    const meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+    const dias = ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'];
+    const mes = meses[fecha.getMonth()];
+    const dia = dias[fecha.getDay()];
+    const diaN = fecha.getDate();
+    const horas = (String(fecha.getHours()).length == 2) ? fecha.getHours() : `0${fecha.getHours()}`;
+    const minutos = (String(fecha.getMinutes()).length == 2) ? fecha.getMinutes() : `0${fecha.getMinutes()}`;
+    const segundos = (String(fecha.getSeconds()).length == 2) ? fecha.getSeconds() : `0${fecha.getSeconds()}`;
+
+    //Mostrando fecha
+    document.getElementById('dia').innerHTML = dia;
+    document.getElementById('mes').innerHTML = `${diaN} ${mes}`;
+    document.querySelector('#hora p:nth-child(1)').innerHTML = horas;
+    document.querySelector('#hora p:nth-child(2)').innerHTML = minutos;
+    document.querySelector('#hora p:nth-child(3)').innerHTML = segundos;
+}, 1000);
